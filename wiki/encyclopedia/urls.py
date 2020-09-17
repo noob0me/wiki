2f45a60e7d560cd1daaa33a666dd205b17c7616d
noob0me <patkaush@gmail.com>
2020-09-17 21:03:55 +0530

    completed

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("edit/<str:name>",views.changes,name = "edit_page"),
     path("new/", views.page, name="new_page"),
-    path("edit/<str:name>",views.page,name = "edit_page"),
     path("random/",views.rand,name = "random_page"),
     path("wiki/<str:name>",views.index,name = "search")
 
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse as hres,HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.http import HttpResponse as hres, HttpResponseRedirect
 from . import util
 from django import forms
 from django.template import loader
@@ -14,68 +14,67 @@ class inputfield(forms.Form):
     context = forms.CharField(label="Information", widget=forms.Textarea(
         attrs={"placeholder": "Enter Description", "id": "bbox"}))
 
-def search(name):
 
-    temp = util.get_entry(name)   #exact word
+def search(name):
+    temp = util.get_entry(name)  # exact word
     if temp != None:
-        return temp,1
-    else:   #matches
+        return temp, 1
+    else:  # matches
         arr = []
         for i in util.list_entries():
 
             if name in i:
                 arr += [i]
-        return arr,0
+        return arr, 0
 
 
 def index(request, name=False):
-    body, names = False,False
-    compare = lambda x: md.markdown(x[0]) if x[1] ==1 else False
+    body, names = False, False
+    compare = lambda x: md.markdown(x[0]) if x[1] == 1 else False
     if name != False:
 
-        names,found = search(name)
-        body = compare([names,found])
+        names, found = search(name)
+        body = compare([names, found])
 
 
     else:
 
         try:
             names, found = search(request.GET["q"])
-            body = compare([names,found])
+            body = compare([names, found])
         except:
             names = util.list_entries()
 
     if len(names) == 0:
-        template = loader.get_template("encyclopedia/error.html")
-        return hres(template.render())
+        return render(request,"encyclopedia/error.html")
+
     return render(request, "encyclopedia/index.html", {
         "entries": names,
-        "val": body ,
-        "title":name
+        "val": body,
+        "title": name
     })
 
 
 # newform(   name,  class, placeholde )
 def rand(request):
     names = util.list_entries()
-    t = random.randint(0,len(names)-1)
-    print(names[t])
-    #return index(request,names[t])
-    return redirect("search",name = names[t])
+    t = random.randint(0, len(names) - 1)
+    return redirect("search", name=names[t])
 
 
-
-
-def page(request,name = False):
+def page(request, name=False):
     x = inputfield()
-    print(type(request.GET))
     same = False
     if request.method == "POST":
         try:
-            if request.POST["title"].upper() not in  util.list_entries():
-                util.save_entry(request.POST["title"],request.POST["context"])
+            if request.POST["title"] not in util.list_entries():
+                util.save_entry(request.POST["title"], request.POST["context"])
                 return HttpResponseRedirect(reverse("index"))
             else:
+                print(request.path)
+                if request.path != "/new/":
+                    util.save_entry(request.POST["title"], request.POST["context"])
+                    return redirect("search", name=request.POST["title"])
                 same = True
                 x = inputfield(request.POST)
 
@@ -84,8 +83,26 @@ def page(request,name = False):
         except:
             pass
     else:
-        x = inputfield(initial={"title":name,"context":util.get_entry(name)})
+        if name:
+            x = inputfield(initial={"title": name, "context": util.get_entry(name)})
     return render(request, "encyclopedia/forms.html", {
-        "form":x,
-        "same":same
+        "form": x,
+        "same": same,
+        "link":False
+
+    })
+
+
+def changes(request, name):
+    if request.method == "POST":
+        util.save_entry(request.POST["title"], request.POST["context"])
+        return HttpResponseRedirect(reverse("index"))
+
+    x = inputfield(initial={"title": name, "context": util.get_entry(name)})
+
+    return render(request, "encyclopedia/edit.html", {
+        "form": x,
+        "title":name ,
+        "link":"edit_page"
+
     })
